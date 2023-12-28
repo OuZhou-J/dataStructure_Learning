fc19139bc8e8dc5c51d7ccd47e4317a2227cfb78
OuZhou-J <jbo20030322@icloud.com>
2023-12-28 11:10:05 +0800

    add useStack.h

---
+++ stack/useStack.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+#include "myStack.h"
+using namespace std;
+void InfixToPostfix(string &zhongzhui, string &houzhui);
+int InfixToPostfixWithCalculate(const string &zhongzhui);
+bool check(std::vector<int> &in, std::vector<int> &out);
