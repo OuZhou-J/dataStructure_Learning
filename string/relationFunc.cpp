30b46ee5b8377c66047ad062898fad0b1ace859c
OuZhou-J <jbo20030322@icloud.com>
2023-12-29 11:45:58 +0800

    add string fold

---
+++ string/relationFunc.cpp
@@ -0,0 +1,70 @@
+#include "relationFunc.h"
+
+//kmp
+//get next array
+static vector<int> getNext(string &s)
+{
+    vector<int> next(s.size(), 0);
+    int k = next[0] = -1;
+    unsigned int loc = 0;
+    while (loc < s.size() - 1) {
+        if (k == -1 || s[loc] == s[k]) {
+            next[++loc] = ++k;
+        } else {
+            k = next[k];
+        }
+    }
+    return next;
+}
+
+void kmp(string &s, string &p)
+{
+    vector<int> next = getNext(p);
+    #ifdef DEBUG
+    for (auto i : next) {
+        cout << i << " ";
+    }
+    cout << endl;
+    #endif
+    unsigned int i = 0, j = 0;
+    while (i < s.size() && j < p.size()) {
+        if (j == -1 || s[i] == p[j]) {
+            ++i;
+            ++j;
+        } else {
+            j = next[j];
+        }
+    }
+    if (j == p.size()) {
+        cout << "find it at " << i - j << endl;
+    } else {
+        cout << "not find" << endl;
+    }
+}
+
+
+int kmpCount(string &s, string &p)
+{
+    vector<int> next = getNext(p);
+    #ifdef DEBUG
+    for (auto i : next) {
+        cout << i << " ";
+    }
+    cout << endl;
+    #endif
+    unsigned int i = 0, j = 0;
+    int count = 0;
+    while (i < s.size()) {
+        if (j == -1 || s[i] == p[j]) {
+            ++i;
+            ++j;
+        } else {
+            j = next[j];
+        }
+        if (j == p.size()) {
+            ++count;
+            j = next[j];//装作失配，继续匹配
+        }
+    }
+    return count;
+}
---
+++ string/relationFunc.h
@@ -0,0 +1,19 @@
+#ifndef RELATIONFUNC_H
+#define RELATIONFUNC_H
+
+#include <vector>
+#include <string>
+#include <functional>
+#include <iostream>
+using namespace std;
+
+//kmp
+void kmp(string &s, string &p);
+
+/*给定目标串S和模式串P，输出P在S中出现的次数，以及P在S中的所有
+匹配位置*/
+int kmpCount(string &s, string &p); //返回匹配次数
+
+
+
+#endif
